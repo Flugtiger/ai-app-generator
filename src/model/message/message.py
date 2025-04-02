6bd36c00739ba0ca84d1695020920fb39cb91053
Alexander Lehmann <a.lehm@gmx.de>
2025-04-02 17:15:06 +0200

    fix: Message doesn't need an ID

--- src/model/message/message.py
+++ src/model/message/message.py
@@ -2,8 +2,6 @@ from enum import Enum
 from pydantic import BaseModel, Field
 from typing import Optional
 
-from src.model.message.message_id import MessageId
-
 
 class MessageRole(str, Enum):
     """
@@ -18,6 +16,5 @@ class Message(BaseModel):
     """
     Value object representing a message in a conversation with an LLM.
     """
-    id: Optional[MessageId] = None
     role: MessageRole = Field(..., description="The role of the message sender")
     content: str = Field(..., description="The content of the message")
